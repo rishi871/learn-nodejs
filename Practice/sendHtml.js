0e656de67212504d21a79b35b5658e7a74a918d5
Rishi Jain <jainrishi871@gmail.com>
2025-06-25 15:00:32 +0530

    Practice: Send HTML as response using readStream and Pipe

---
+++ Practice/sendHtml.js
@@ -0,0 +1,14 @@
+let http = require('http');
+let fs = require('fs');
+
+let server = http.createServer(function(req,res) {
+
+    console.log('request was made: ' + req.url );
+    res.writeHead(200, {'Content-Type': 'text/Html'});
+    let myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+    myReadStream.pipe(res);
+
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('listeningin to port 3000')
